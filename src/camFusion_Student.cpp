c98e1427b59928d8fc83e627114e4660884a3428
mahmoudzayed <mahmoud.moataz.zayed@gmail.com>
2020-04-16 21:27:54 +0200

    Update camFusion_Student.cpp

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <algorithm>
 #include <numeric>
@@ -10,130 +9,141 @@
 
 using namespace std;
 
+static double calcMedian(std::vector<LidarPoint> &lidarPoints, int liderPtsize, int offset);
+static double filteroutliers(std::vector<LidarPoint> &lidarPoints);
 
 // Create groups of Lidar points whose projection into the camera falls into the same bounding box
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints, float shrinkFactor, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT)
 {
-    // loop over all Lidar points and associate them to a 2D bounding box
-    cv::Mat X(4, 1, cv::DataType<double>::type);
-    cv::Mat Y(3, 1, cv::DataType<double>::type);
+  // loop over all Lidar points and associate them to a 2D bounding box
+  cv::Mat X(4, 1, cv::DataType<double>::type);
+  cv::Mat Y(3, 1, cv::DataType<double>::type);
 
-    for (auto it1 = lidarPoints.begin(); it1 != lidarPoints.end(); ++it1)
+  for (auto it1 = lidarPoints.begin(); it1 != lidarPoints.end(); ++it1)
+  {
+    // assemble vector for matrix-vector-multiplication
+    X.at<double>(0, 0) = it1->x;
+    X.at<double>(1, 0) = it1->y;
+    X.at<double>(2, 0) = it1->z;
+    X.at<double>(3, 0) = 1;
+
+    // project Lidar point into camera
+    Y = P_rect_xx * R_rect_xx * RT * X;
+    cv::Point pt;
+    pt.x = Y.at<double>(0, 0) / Y.at<double>(0, 2); // pixel coordinates
+    pt.y = Y.at<double>(1, 0) / Y.at<double>(0, 2);
+
+    vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
+    for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
     {
-        // assemble vector for matrix-vector-multiplication
-        X.at<double>(0, 0) = it1->x;
-        X.at<double>(1, 0) = it1->y;
-        X.at<double>(2, 0) = it1->z;
-        X.at<double>(3, 0) = 1;
+      // shrink current bounding box slightly to avoid having too many outlier points around the edges
+      cv::Rect smallerBox;
+      smallerBox.x = (*it2).roi.x + shrinkFactor * (*it2).roi.width / 2.0;
+      smallerBox.y = (*it2).roi.y + shrinkFactor * (*it2).roi.height / 2.0;
+      smallerBox.width = (*it2).roi.width * (1 - shrinkFactor);
+      smallerBox.height = (*it2).roi.height * (1 - shrinkFactor);
 
-        // project Lidar point into camera
-        Y = P_rect_xx * R_rect_xx * RT * X;
-        cv::Point pt;
-        pt.x = Y.at<double>(0, 0) / Y.at<double>(0, 2); // pixel coordinates
-        pt.y = Y.at<double>(1, 0) / Y.at<double>(0, 2);
+      // check wether point is within current bounding box
+      if (smallerBox.contains(pt))
+      {
+        enclosingBoxes.push_back(it2);
+      }
 
-        vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
-        for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
-        {
-            // shrink current bounding box slightly to avoid having too many outlier points around the edges
-            cv::Rect smallerBox;
-            smallerBox.x = (*it2).roi.x + shrinkFactor * (*it2).roi.width / 2.0;
-            smallerBox.y = (*it2).roi.y + shrinkFactor * (*it2).roi.height / 2.0;
-            smallerBox.width = (*it2).roi.width * (1 - shrinkFactor);
-            smallerBox.height = (*it2).roi.height * (1 - shrinkFactor);
+    } // eof loop over all bounding boxes
 
-            // check wether point is within current bounding box
-            if (smallerBox.contains(pt))
-            {
-                enclosingBoxes.push_back(it2);
-            }
+    // check wether point has been enclosed by one or by multiple boxes
+    if (enclosingBoxes.size() == 1)
+    { 
+      // add Lidar point to bounding box
+      enclosingBoxes[0]->lidarPoints.push_back(*it1);
+    }
 
-        } // eof loop over all bounding boxes
-
-        // check wether point has been enclosed by one or by multiple boxes
-        if (enclosingBoxes.size() == 1)
-        { 
-            // add Lidar point to bounding box
-            enclosingBoxes[0]->lidarPoints.push_back(*it1);
-        }
-
-    } // eof loop over all Lidar points
+  } // eof loop over all Lidar points
 }
 
 
 void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait)
 {
-    // create topview image
-    cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
+  // create topview image
+  cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    for(auto it1=boundingBoxes.begin(); it1!=boundingBoxes.end(); ++it1)
+  for(auto it1=boundingBoxes.begin(); it1!=boundingBoxes.end(); ++it1)
+  {
+    // create randomized color for current 3D object
+    cv::RNG rng(it1->boxID);
+    cv::Scalar currColor = cv::Scalar(rng.uniform(0,150), rng.uniform(0, 150), rng.uniform(0, 150));
+
+    // plot Lidar points into top view image
+    int top=1e8, left=1e8, bottom=0.0, right=0.0; 
+    float xwmin=1e8, ywmin=1e8, ywmax=-1e8;
+    for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
     {
-        // create randomized color for current 3D object
-        cv::RNG rng(it1->boxID);
-        cv::Scalar currColor = cv::Scalar(rng.uniform(0,150), rng.uniform(0, 150), rng.uniform(0, 150));
+      // world coordinates
+      float xw = (*it2).x; // world position in m with x facing forward from sensor
+      float yw = (*it2).y; // world position in m with y facing left from sensor
+      xwmin = xwmin<xw ? xwmin : xw;
+      ywmin = ywmin<yw ? ywmin : yw;
+      ywmax = ywmax>yw ? ywmax : yw;
 
-        // plot Lidar points into top view image
-        int top=1e8, left=1e8, bottom=0.0, right=0.0; 
-        float xwmin=1e8, ywmin=1e8, ywmax=-1e8;
-        for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
-        {
-            // world coordinates
-            float xw = (*it2).x; // world position in m with x facing forward from sensor
-            float yw = (*it2).y; // world position in m with y facing left from sensor
-            xwmin = xwmin<xw ? xwmin : xw;
-            ywmin = ywmin<yw ? ywmin : yw;
-            ywmax = ywmax>yw ? ywmax : yw;
+      // top-view coordinates
+      int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
+      int x = (-yw * imageSize.width / worldSize.width) + imageSize.width / 2;
 
-            // top-view coordinates
-            int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
-            int x = (-yw * imageSize.width / worldSize.width) + imageSize.width / 2;
+      // find enclosing rectangle
+      top = top<y ? top : y;
+      left = left<x ? left : x;
+      bottom = bottom>y ? bottom : y;
+      right = right>x ? right : x;
 
-            // find enclosing rectangle
-            top = top<y ? top : y;
-            left = left<x ? left : x;
-            bottom = bottom>y ? bottom : y;
-            right = right>x ? right : x;
-
-            // draw individual point
-            cv::circle(topviewImg, cv::Point(x, y), 4, currColor, -1);
-        }
-
-        // draw enclosing rectangle
-        cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom),cv::Scalar(0,0,0), 2);
-
-        // augment object with some key data
-        char str1[200], str2[200];
-        sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
-        putText(topviewImg, str1, cv::Point2f(left-250, bottom+50), cv::FONT_ITALIC, 2, currColor);
-        sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax-ywmin);
-        putText(topviewImg, str2, cv::Point2f(left-250, bottom+125), cv::FONT_ITALIC, 2, currColor);  
+      // draw individual point
+      cv::circle(topviewImg, cv::Point(x, y), 4, currColor, -1);
     }
 
-    // plot distance markers
-    float lineSpacing = 2.0; // gap between distance markers
-    int nMarkers = floor(worldSize.height / lineSpacing);
-    for (size_t i = 0; i < nMarkers; ++i)
-    {
-        int y = (-(i * lineSpacing) * imageSize.height / worldSize.height) + imageSize.height;
-        cv::line(topviewImg, cv::Point(0, y), cv::Point(imageSize.width, y), cv::Scalar(255, 0, 0));
-    }
+    // draw enclosing rectangle
+    cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom),cv::Scalar(0,0,0), 2);
 
-    // display image
-    string windowName = "3D Objects";
-    cv::namedWindow(windowName, 1);
-    cv::imshow(windowName, topviewImg);
+    // augment object with some key data
+    char str1[200], str2[200];
+    sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
+    putText(topviewImg, str1, cv::Point2f(left-250, bottom+50), cv::FONT_ITALIC, 2, currColor);
+    sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax-ywmin);
+    putText(topviewImg, str2, cv::Point2f(left-250, bottom+125), cv::FONT_ITALIC, 2, currColor);  
+  }
 
-    if(bWait)
-    {
-        cv::waitKey(0); // wait for key to be pressed
-    }
+  // plot distance markers
+  float lineSpacing = 2.0; // gap between distance markers
+  int nMarkers = floor(worldSize.height / lineSpacing);
+  for (size_t i = 0; i < nMarkers; ++i)
+  {
+    int y = (-(i * lineSpacing) * imageSize.height / worldSize.height) + imageSize.height;
+    cv::line(topviewImg, cv::Point(0, y), cv::Point(imageSize.width, y), cv::Scalar(255, 0, 0));
+  }
+
+  // display image
+  string windowName = "3D Objects";
+  cv::namedWindow(windowName, 1);
+  cv::imshow(windowName, topviewImg);
+
+  if(bWait)
+  {
+    cv::waitKey(0); // wait for key to be pressed
+  }
 }
 
 
 // associate a given bounding box with the keypoints it contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // ...
+//   std::cout<<"kptMatches: "<<kptMatches.size()<<std::endl;
+  for(auto it = kptMatches.begin(); it != kptMatches.end(); ++it)
+  {
+
+    if(boundingBox.roi.contains(kptsPrev[it->queryIdx].pt) && boundingBox.roi.contains(kptsCurr[it->trainIdx].pt))
+    {
+      boundingBox.kptMatches.push_back(*it);
+      boundingBox.keypoints.push_back(kptsCurr[it->trainIdx]);
+    }
+  }
 }
 
 
@@ -141,18 +151,147 @@ void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, 
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg)
 {
-    // ...
+  // compute distance ratios between all matched keypoints
+  vector<double> distRatios; // stores the distance ratios for all keypoints between curr. and prev. frame
+  for (auto it1 = kptMatches.begin(); it1 != kptMatches.end() - 1; ++it1)
+  { // outer kpt. loop
+
+    // get current keypoint and its matched partner in the prev. frame
+    cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
+    cv::KeyPoint kpOuterPrev = kptsPrev.at(it1->queryIdx);
+
+    for (auto it2 = kptMatches.begin() + 1; it2 != kptMatches.end(); ++it2)
+    { // inner kpt.-loop
+
+      double minDist = 100.0; // min. required distance
+
+      // get next keypoint and its matched partner in the prev. frame
+      cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
+      cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
+
+      // compute distances and distance ratios
+      double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
+      double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
+
+      if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist)
+      { // avoid division by zero
+
+        double distRatio = distCurr / distPrev;
+        distRatios.push_back(distRatio);
+
+      }
+    } // eof inner loop over all matched kpts
+  }     // eof outer loop over all matched kpts
+
+  // only continue if list of distance ratios is not empty
+  if (distRatios.size() == 0)
+  {
+    TTC = NAN;
+    return;
+  }   
+  
+  double medianDistRatio;
+  std::sort(distRatios.begin(), distRatios.end());
+  int distratiosize = distRatios.size();
+  if(distratiosize%2 ==0)
+  {
+    int middle = distratiosize / 2.0;
+    medianDistRatio = (distRatios[middle-1] + distRatios[middle])/2;
+  }
+  else
+  {
+    medianDistRatio = distRatios[distratiosize/2.0];
+  }
+  
+
+  double dT = 1 / frameRate;
+  TTC = dT / abs(medianDistRatio - 1);
+//   std::cout<<"cameraTTC: "<<TTC<<"median.: "<<medianDistRatio<<std::endl;
 }
 
+static double calcMedian(std::vector<LidarPoint> &lidarPoints, int liderPtsize, int offset)
+{
+  double median;
+  if(liderPtsize%2 == 0)
+  {
+    median = 0.5 * (lidarPoints[offset + (liderPtsize / 2) - 1].x + lidarPoints[offset + (liderPtsize / 2)].x);;
+  }
+  else
+  {
+    median = lidarPoints[offset + (liderPtsize / 2)].x;
+  }
+  return median;
+}
+
+static double filteroutliers(std::vector<LidarPoint> &lidarPoints)
+{
+  double median, medianQ1, medianQ3, IQR;
+  double minX = 1e9;
+
+  median = calcMedian(lidarPoints, lidarPoints.size(), 0);
+  medianQ1 = calcMedian(lidarPoints, lidarPoints.size()/2, 0);
+  medianQ3 = calcMedian(lidarPoints, lidarPoints.size()/2, std::ceil(lidarPoints.size()/2));
+  IQR = abs(medianQ3 - medianQ1);
+
+//   std::cout<<"median: "<<median<<", "<<medianQ1<<", "<<medianQ3<<", "<<IQR<<std::endl;
+
+  //   std::cout<<"pts: ";
+  for (auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
+  {
+    if(it->x > (std::min(medianQ1, medianQ3) - 1.5*IQR))
+    {
+      minX = minX > it->x ? it->x : minX;
+    }
+    //     std::cout<<", "<<it->x;
+  }
+  //   std::cout<<std::endl;
+//   std::cout<<"minX: "<<minX<<", median: "<<median<<std::endl;
+  return minX;
+
+}
 
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
 {
-    // ...
+  // find closest distance to Lidar points within ego lane
+  double minXPrev  = 1e9, minXCurr = 1e9;
+  std::sort(lidarPointsPrev.begin(), lidarPointsPrev.end(), by_x());
+  std::sort(lidarPointsCurr.begin(), lidarPointsCurr.end(), by_x());
+
+  minXPrev = filteroutliers(lidarPointsPrev);
+  minXCurr = filteroutliers(lidarPointsCurr);
+
+  // compute TTC from both measurements
+  double dT = 1 / frameRate;
+  TTC = minXCurr * dT / (minXPrev - minXCurr);
+//   std::cout<<"lidarTTC: "<<TTC<<", dT: "<<dT<<", minXCurr: "<<minXCurr<<", minXPrev: "<<minXPrev<<std::endl;
 }
 
 
 void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, DataFrame &prevFrame, DataFrame &currFrame)
 {
-    // ...
+  for(auto it1 = currFrame.boundingBoxes.begin(); it1 != currFrame.boundingBoxes.end(); ++it1)
+  {
+    // we create a counter for each previous box
+    std::vector<int> boxmatch(prevFrame.boundingBoxes.size(),0);
+    for(auto it2 = matches.begin(); it2 != matches.end(); ++it2)
+    {
+      for(auto it3 = prevFrame.boundingBoxes.begin(); it3 != prevFrame.boundingBoxes.end(); ++it3)
+      {
+        if(it3->roi.contains(prevFrame.keypoints[it2->queryIdx].pt) && it1->roi.contains(currFrame.keypoints[it2->trainIdx].pt))
+        {
+          boxmatch[it3->boxID]++;
+        }
+      }
+      //std::cout<<"Keypoints: "<<prevFrame.keypoints[0].pt<<", "<<currFrame.keypoints[0].pt;
+      //std::cout<<" Bboxes: "<<prevFrame.boundingBoxes[0].boxID<<", "<< currFrame.boundingBoxes[0].boxID<<std::endl;
+    }
+    int maxElementIndex = std::max_element(boxmatch.begin(),boxmatch.end()) - boxmatch.begin();
+    if(boxmatch[maxElementIndex] > 0)
+    {
+      bbBestMatches.insert({maxElementIndex,it1->boxID});
+//       std::cout<<"bbBestMatches: "<<it1->boxID<<", "<<maxElementIndex<<", "<<boxmatch[maxElementIndex]<<std::endl;
+    }
+  }
+  // todo remove the multiple association 
 }
